5eccf8ff6a495a7b589d42ecb7a2db80d53b9aa6
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-20 11:06:10 +0600

    Two Problem SOlved

---
+++ index.js
@@ -0,0 +1,45 @@
+// 01. Remove Duplicates from Sorted Array
+/**
+* @param {number[]} nums
+* @return {number}
+*/
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return 0;
+
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+
+
+// 02. Binary Search
+/**
+* @param {number[]} nums
+* @param {number} target
+* @return {number}
+*/
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+
