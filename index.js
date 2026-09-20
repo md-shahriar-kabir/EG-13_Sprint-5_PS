4b676ffd3be164032d44755eccd147287b4f51f2
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-20 11:09:37 +0600

    5 & 6  Problem Soloved

--- index.js
+++ index.js
@@ -80,3 +80,45 @@ var maxDepth = function (root) {
     if (!root) return 0;
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
+
+
+
+// 05. Invert Binary Tree
+/**
+* @param {TreeNode} root
+* @return {TreeNode}
+*/
+var invertTree = function (root) {
+    if (!root) return null;
+    const left = invertTree(root.left);
+    const right = invertTree(root.right);
+    root.left = right;
+    root.right = left;
+    return root;
+};
+
+
+
+// 06. Product of Array Except Self
+/**
+* @param {number[]} nums
+* @return {number[]}
+*/
+var productExceptSelf = function (nums) {
+    const n = nums.length;
+    const answer = new Array(n);
+
+    answer[0] = 1;
+    for (let i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] = answer[i] * rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return answer;
+};
+
