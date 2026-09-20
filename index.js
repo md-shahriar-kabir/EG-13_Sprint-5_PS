d930a87a8daa08840073bee6074752f10378c9bd
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-20 11:08:58 +0600

    3 & 4 Problem Soloved

--- index.js
+++ index.js
@@ -43,3 +43,40 @@ var search = function (nums, target) {
     return -1;
 };
 
+
+
+// 03. Search Insert Position
+/**
+* @param {number[]} nums
+* @param {number} target
+* @return {number}
+*/
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+
+
+// 04. Maximum Depth of Binary Tree
+/**
+* @param {TreeNode} root
+* @return {number}
+*/
+var maxDepth = function (root) {
+    if (!root) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
